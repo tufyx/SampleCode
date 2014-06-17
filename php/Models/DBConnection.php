a2c3ca7f657bae90ed5a26f34d6f27e4f5ffd464
Vlad Tufis <vlad.tufis@gmail.com>
2014-06-17 19:00:42 +0200

    Update DBConnection.php

--- php/Models/DBConnection.php
+++ php/Models/DBConnection.php
@@ -3,7 +3,6 @@ namespace Models;
 class DBConnection {
 	
 	function __construct() {
-		$connection = array();
 		$dbconnect = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
 		mysql_select_db(DB_NAME) or die(mysql_error());
 	}
